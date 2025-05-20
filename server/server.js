95b360e2d4369a67f5f3b4f6d7932ab4886f0692
mohan-bee <mohn08052006@gmail.com>
2025-05-20 11:27:08 +0530

    aww

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(passport.session());
 
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://test-auth-ebon.vercel.app",
 		methods: "GET,POST,PUT,DELETE",
 		credentials: true,
 	})
